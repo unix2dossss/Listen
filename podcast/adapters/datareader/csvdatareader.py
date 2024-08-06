1a240bd8cdd77a55f19efa64f882a32abbda97a4
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-08-07 11:15:27 +1200

    Create Podcasts Function 1/2 implementation

--- podcast/adapters/datareader/csvdatareader.py
+++ podcast/adapters/datareader/csvdatareader.py
@@ -23,8 +23,22 @@ class CSVDataReader:
                 row = [item.strip() for item in row]
                 yield row
 
+    def create_podcasts(self):
+        for row in self.read_csv("../data/podcasts.csv"):
+            # pc = podcast - using these variables as keyword arguments for better readability
+            pc_id, pc_title = int(row[0]), row[1]
+            pc_image, pc_description = row[2], row[3]
+            pc_language = row[4]
+            pc_categories, pc_website = row[5], row[6]
+            pc_author, pc_itunes_id = row[7], int(row[8])
 
-
+            new_podcast = Podcast(
+                podcast_id=pc_id, title=pc_title,
+                image=pc_image, description=pc_description,
+                language=pc_language,
+                website=pc_website, author=pc_author,
+                itunes_id= pc_itunes_id
+            )
 
 
 a = CSVDataReader()
