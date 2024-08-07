6108b517575c98e556bc09970f196483c355586a
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-08-07 14:45:12 +1200

    Create Episode Object

--- podcast/adapters/datareader/csvdatareader.py
+++ podcast/adapters/datareader/csvdatareader.py
@@ -1,6 +1,8 @@
 import os
 import csv
-from podcast.domainmodel.model import Podcast, Episode, Author, Category
+from datetime import datetime
+
+from podcast.domainmodel.model import Podcast, Episode, Author, Category, AudioTime
 
 
 class CSVDataReader:
@@ -9,10 +11,11 @@ class CSVDataReader:
         self._authors, self._categories = dict(), dict()
         self._podcasts_by_category = {}
 
-        self.create_podcasts()
+        self.create_podcasts("../data/podcasts.csv")
+        self.create_episodes("../data/podcasts.csv")
 
-    def read_csv(self, episodes_file: str):
-        with open(episodes_file, mode='r') as file_in:
+    def read_csv(self, input_file: str):
+        with open(input_file, mode='r') as file_in:
             csv_data = csv.reader(file_in, delimiter=',')
             headers = next(csv_data)  # read the headers
 
@@ -21,59 +24,90 @@ class CSVDataReader:
                 row = [item.strip() for item in row]
                 yield row
 
-    def create_podcasts(self):
+    def create_podcasts(self, podcast_file):
         a_id = 1
-        for row in self.read_csv("../data/podcasts.csv"):
-            # pc = podcast - using these variables as keyword arguments for better readability
-            pc_id, pc_title = int(row[0]), row[1]
-            pc_image, pc_description = row[2], row[3]
-            pc_language = row[4]
-            pc_categories, pc_website = row[5], row[6]
-            pc_author, pc_itunes_id = row[7], int(row[8])
+        try:
+            for row in self.read_csv(podcast_file):
+                # pc = podcast - using these variables as keyword arguments for better readability
+                pc_id, pc_title = int(row[0]), row[1]
+                pc_image, pc_description = row[2], row[3]
+                pc_language = row[4]
+                pc_categories, pc_website = row[5], row[6]
+                pc_author, pc_itunes_id = row[7], int(row[8])
 
-            # Create Author Object if it doesn't already exist
-            if pc_author not in self._authors:
-                author = Author(
-                    author_id=a_id,
-                    name=pc_author if len(pc_author) > 0 else "Author Not Provided"
-                )
-                a_id += 1
-                self._authors[pc_author] = author
-
-            # Create Podcast Object
-            new_podcast = Podcast(
-                podcast_id=pc_id, title=pc_title,
-                image=pc_image, description=pc_description,
-                language=pc_language,
-                website=pc_website, author=self._authors[pc_author],
-                itunes_id=pc_itunes_id
-            )
-
-            # Create Categories
-            c_id = 1
-            for c in pc_categories.split(' | '):
-                category: Category = None
-                if c in self._categories:
-                    category = self._categories[c]
-                else:
-                    category = Category(
-                        category_id=c_id,
-                        name=c
+                # Create Author Object if it doesn't already exist
+                if pc_author not in self._authors:
+                    author = Author(
+                        author_id=a_id,
+                        name=pc_author if len(pc_author) > 0 else "Author Not Provided"
                     )
-                    c_id += 1
-                    self._categories[c] = category
+                    a_id += 1
+                    self._authors[pc_author] = author
 
-                if c in self._podcasts_by_category:
-                    self._podcasts_by_category[c].append(new_podcast)
-                else:
-                    self._podcasts_by_category[c] = [new_podcast]
+                # Create Podcast Object
+                new_podcast = Podcast(
+                    podcast_id=pc_id, title=pc_title,
+                    image=pc_image, description=pc_description,
+                    language=pc_language,
+                    website=pc_website, author=self._authors[pc_author],
+                    itunes_id=pc_itunes_id
+                )
 
-                # Add Category to Podcast
-                new_podcast.add_category(category)
+                # Create Categories
+                c_id = 1
+                for c in pc_categories.split(' | '):
+                    category: Category = None
+                    if c in self._categories:
+                        category = self._categories[c]
+                    else:
+                        category = Category(
+                            category_id=c_id,
+                            name=c
+                        )
+                        c_id += 1
+                        self._categories[c] = category
 
-            # Add podcast to authors podcast list
-            self._authors[pc_author].add_podcast(new_podcast)
-            self._podcasts.append(new_podcast)
+                    if c in self._podcasts_by_category:
+                        self._podcasts_by_category[c].append(new_podcast)
+                    else:
+                        self._podcasts_by_category[c] = [new_podcast]
 
+                    # Add Category to Podcast
+                    new_podcast.add_category(category)
+
+                # Add podcast to authors podcast list
+                self._authors[pc_author].add_podcast(new_podcast)
+                self._podcasts.append(new_podcast)
+        except ValueError as e:
+            print(f"Skipping row (invalid data): {e}")
+
+    def create_episodes(self, episode_file: str):
+        a_id = 1
+        try:
+            for row in self.read_csv(episode_file):
+                # ep = episode
+                ep_id, pc_id = int(row[0]), row[1]
+                ep_title, ep_audio_link = row[2], row[3]
+                ep_audio_length = row[4]
+                ep_description, ep_pub_date = row[5], row[6]
+
+                ep_pub_date_object = datetime.strptime(ep_pub_date, "%Y-%m-%d %H:%M:%S%z")
+
+                hours = int(ep_audio_length) // 3600
+                minutes = (int(ep_audio_length) % 3600) // 60
+                seconds = int(ep_audio_length) % 60
+                ep_audio_length_object = AudioTime(hours, minutes, seconds)
+
+                # Create Episode Object
+                new_podcast = Episode(
+                    episode_id=ep_id, episode_title=ep_title,
+                    episode_audio_link=ep_audio_link,
+                    episode_audio_length=ep_audio_length_object,
+                    episode_description=ep_description,
+                    episode_publish_date=ep_pub_date_object
+                )
+
+        except Exception as e:
+            print(f"Skipping row (invalid data): {e}")
 
 a = CSVDataReader()
