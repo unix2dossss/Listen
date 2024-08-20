cacf0319124ef493cd68c978ed72ec807936116f
daisy-of-the-valley <deizat1111@gmail.com>
2024-08-20 23:22:34 +1200

    Fix routing on homepage and homepage header

--- podcast/home/home.py
+++ podcast/home/home.py
@@ -7,11 +7,17 @@ home_blueprint = Blueprint(
     'home_bp', __name__)
 
 
-@home_blueprint.route('/', methods=['GET'])
-def home():
-    # url = url_for('home_bp.podcasts_by_facet', facet_name="top_podcasts")
-    # return redirect(url)
-    display_podcasts = services.get_top_podcasts(repo.repo_instance)
+@home_blueprint.route('/', defaults={'facet_name': 'top_podcasts'}, methods=['GET'])
+@home_blueprint.route('/<facet_name>', methods=['GET'])
+def home(facet_name):
+
+    if facet_name == 'recently_played':
+        display_podcasts = services.get_recently_played(repo.repo_instance)
+    elif facet_name == 'new_podcasts':
+        display_podcasts = services.get_new_podcasts(repo.repo_instance)
+    else:
+        display_podcasts = services.get_top_podcasts(repo.repo_instance)
+
     continue_listening_list = services.get_continue_listening_podcasts(repo.repo_instance)
     top_authors = services.get_top_authors(repo.repo_instance)
 
@@ -20,24 +26,6 @@ def home():
         facet_podcasts=display_podcasts,
         continue_listening=continue_listening_list,
         top_authors=top_authors,
-    )
-
-
-@home_blueprint.route('/<facet_name>', methods=['GET'])
-def podcasts_by_facet(facet_name):
-    display_podcasts = services.get_top_podcasts(repo.repo_instance)
-    continue_listening_list = services.get_continue_listening_podcasts(repo.repo_instance)
-    top_authors=services.get_top_authors(repo.repo_instance)
-
-    if facet_name == 'recently_played':
-        display_podcasts = services.get_recently_played(repo.repo_instance)
-    elif facet_name == 'new_podcasts':
-        display_podcasts = services.get_new_podcasts(repo.repo_instance)
-
-    return render_template(
-        'home/home.html',
-        facet_podcasts=display_podcasts,
-        continue_listening=continue_listening_list,
-        top_authors=top_authors,
         facet_name=facet_name
     )
+
