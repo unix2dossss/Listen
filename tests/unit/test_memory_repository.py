fdb74c546d0b579d02f354d09e78429a15567543
daisy-of-the-valley <deizat1111@gmail.com>
2024-08-23 21:20:14 +1200

    Write test to retrive popular categories

--- tests/unit/test_memory_repository.py
+++ tests/unit/test_memory_repository.py
@@ -12,5 +12,7 @@ def test_repository_can_retrieve_a_podcast_by_id(in_memory_repo):
     podcast = in_memory_repo.get_podcast(id_no)
     assert podcast == in_memory_repo.podcasts[0]
 
-
-
+def test_repository_can_retrieve_popular_categories(in_memory_repo):
+    popular_categories = in_memory_repo.get_popular_categories()
+    assert popular_categories == [in_memory_repo.categories.values()[1], in_memory_repo.categories.values()[5],
+                                  in_memory_repo.categories.values()[16]]
