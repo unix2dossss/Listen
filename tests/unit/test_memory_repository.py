b64fd1f9d550e556a1a6a327c872943bb4a88b0c
daisy-of-the-valley <deizat1111@gmail.com>
2024-08-24 21:15:44 +1200

    Fix memory repository tests

--- tests/unit/test_memory_repository.py
+++ tests/unit/test_memory_repository.py
@@ -1,23 +1,3 @@
-from datetime import date, datetime
-from typing import List
-
-import pytest
-# from podcast.adapters.repository import AbstractRepository, MemoryRepository
-# from datareader import AbstractRepository, MemoryRepository
-# from podcast.adapters.memory_repository import MemoryRepository
-
-import podcast.adapters.repository as repo
-from podcast.adapters.memory_repository import MemoryRepository, populate
-
-from podcast.domainmodel.model import User, Podcast
-from podcast.adapters.repository import RepositoryException
-
-@pytest.fixture
-def in_memory_repo():
-    repo = MemoryRepository()
-    return repo
-
-
 def test_repository_can_retrieve_a_podcast_by_id(in_memory_repo):
     id_no = 1
     podcast = in_memory_repo.get_podcast(id_no)
@@ -26,23 +6,26 @@ def test_repository_can_retrieve_a_podcast_by_id(in_memory_repo):
 
 def test_repository_can_retrieve_popular_categories(in_memory_repo):
     popular_categories = in_memory_repo.get_popular_categories()
-    assert popular_categories == [in_memory_repo.categories.values()[1], in_memory_repo.categories.values()[5],
-                                  in_memory_repo.categories.values()[16]]
+    assert popular_categories == [list(in_memory_repo.categories.values())[1],
+                                  list(in_memory_repo.categories.values())[5],
+                                  list(in_memory_repo.categories.values())[16]]
 
 
 def test_can_retrieve_editor_picks(in_memory_repo):
     editor_picks = in_memory_repo.get_editor_picks()
-    assert editor_picks == [[in_memory_repo.podcasts[288], in_memory_repo.podcasts[162], in_memory_repo.podcasts[799],
-                             in_memory_repo.podcasts[317]]]
+    expected_picks = [in_memory_repo.podcasts[106], in_memory_repo.podcasts[503],
+                      in_memory_repo.podcasts[829]]
+
+    assert editor_picks == expected_picks
 
 
 def test_can_get_podcasts_in_specified_category(in_memory_repo):
-    comedy_podcasts = in_memory_repo.get_podcasts_in_category('comedy')
-    assert comedy_podcasts == in_memory_repo.podcasts_by_category['comedy']
+    comedy_podcasts = in_memory_repo.get_podcasts_in_category('Comedy')
+    assert comedy_podcasts == in_memory_repo.podcasts_by_category['Comedy']
 
 
 def test_can_retrieve_podcasts_by_specified_author(in_memory_repo):
-    author_name = "audioboom"
+    author_name = "Audioboom"
     audioboom_podcasts = in_memory_repo.get_podcasts_by_author(author_name)
     assert audioboom_podcasts == in_memory_repo.authors[author_name].podcast_list
 
@@ -54,42 +37,43 @@ def test_can_retrieve_all_podcasts(in_memory_repo):
 
 def test_can_retrieve_all_categories(in_memory_repo):
     all_categories = in_memory_repo.get_all_categories()
-    assert all_categories == in_memory_repo.categories.values()
+    assert all_categories == list(in_memory_repo.categories.values())
 
 
 def test_can_retrieve_all_authors(in_memory_repo):
     all_authors = in_memory_repo.get_all_authors()
-    assert all_authors == in_memory_repo.authors.values()
+    assert all_authors == list(in_memory_repo.authors.values())
 
 
 def test_can_retrieve_top_podcasts(in_memory_repo):
     top_podcasts = in_memory_repo.get_top_podcasts()
-    assert top_podcasts == [in_memory_repo.podcasts[771], in_memory_repo.podcasts[531], in_memory_repo.podcasts[88],
-                            in_memory_repo.podcasts[438]]
+    assert top_podcasts == [in_memory_repo.podcasts[771], in_memory_repo.podcasts[531],
+                            in_memory_repo.podcasts[88], in_memory_repo.podcasts[438]]
 
 
 def test_can_retrieve_recently_played_podcasts(in_memory_repo):
     recent_podcasts = in_memory_repo.get_recently_played()
-    assert recent_podcasts == [in_memory_repo.podcasts[670], in_memory_repo.podcasts[219], in_memory_repo.podcasts[728],
-                               in_memory_repo.podcasts[8]]
+    assert recent_podcasts == [in_memory_repo.podcasts[670], in_memory_repo.podcasts[219],
+                               in_memory_repo.podcasts[728], in_memory_repo.podcasts[8]]
 
 
 def test_can_retrieve_new_podcasts(in_memory_repo):
     new_podcasts = in_memory_repo.get_new_podcasts()
-    assert new_podcasts == [in_memory_repo.podcasts[739], in_memory_repo.podcasts[268], in_memory_repo.podcasts[639],
-                            in_memory_repo.podcasts[200]]
+    assert new_podcasts == [in_memory_repo.podcasts[739], in_memory_repo.podcasts[268],
+                            in_memory_repo.podcasts[639], in_memory_repo.podcasts[200]]
 
 
 def test_can_retrieve_continue_listening_podcasts(in_memory_repo):
     cl_podcasts = in_memory_repo.get_continue_listening_podcasts()
-    assert cl_podcasts == [in_memory_repo.podcasts[546], in_memory_repo.podcasts[823], in_memory_repo.podcasts[908],
-                           in_memory_repo.podcasts[675]]
+    assert cl_podcasts == [in_memory_repo.podcasts[546], in_memory_repo.podcasts[823],
+                           in_memory_repo.podcasts[908], in_memory_repo.podcasts[675]]
 
 
 def test_can_retrieve_top_authors(in_memory_repo):
     top_authors = in_memory_repo.get_top_authors()
-    assert top_authors == [in_memory_repo.authors.values()[22], in_memory_repo.authors.values()[45],
-                           in_memory_repo.authors.values()[52]]
+    assert top_authors == [list(in_memory_repo.authors.values())[22],
+                           list(in_memory_repo.authors.values())[45],
+                           list(in_memory_repo.authors.values())[52]]
 
 
 def test_can_retrieve_list_of_top_podcasts(in_memory_repo):
