00df56246d7f3b010e87a0e2672582e6b9cc645f
daisy-of-the-valley <deizat1111@gmail.com>
2024-10-12 14:31:59 +1300

    Write tests for populating database

--- tests_db/unit/test_populate_database.py
+++ tests_db/unit/test_populate_database.py
@@ -31,14 +31,13 @@ def test_database_populate_select_all_authors(database_engine):
 
 
 def test_database_populate_select_all_categories(database_engine):
-
     # Get table information
     inspector = inspect(database_engine)
-    name_of_authors_table = inspector.get_table_names()[2]
+    name_of_categories_table = inspector.get_table_names()[2]
 
     with database_engine.connect() as connection:
         # query for records in table users
-        select_statement = select([mapper_registry.metadata.tables[name_of_authors_table]])
+        select_statement = select([mapper_registry.metadata.tables[name_of_categories_table]])
         result = connection.execute(select_statement)
 
         all_categories = []
@@ -51,47 +50,42 @@ def test_database_populate_select_all_categories(database_engine):
 
 
 def test_database_populate_select_all_podcasts(database_engine):
-
     # Get table information
     inspector = inspect(database_engine)
-    name_of_authors_table = inspector.get_table_names()[9]
+    name_of_podcasts_table = inspector.get_table_names()[9]
 
     with database_engine.connect() as connection:
         # query for records in table users
-        select_statement = select([mapper_registry.metadata.tables[name_of_authors_table]])
+        select_statement = select([mapper_registry.metadata.tables[name_of_podcasts_table]])
         result = connection.execute(select_statement)
 
         all_podcasts = []
         for row in result:
             all_podcasts.append(row['title'])
 
-
         assert all_podcasts[0] == 'D-Hour Radio Network'
         assert all_podcasts[1] == 'Brian Denny Radio'
-        assert len(all_podcasts) == 1000
+        assert len(all_podcasts) == 2
 
 
 def test_database_populate_select_all_episodes(database_engine):
-
     # Get table information
     inspector = inspect(database_engine)
-    name_of_authors_table = inspector.get_table_names()[5]
+    name_of_episodes_table = inspector.get_table_names()[5]
 
     with database_engine.connect() as connection:
         # query for records in table users
-        select_statement = select([mapper_registry.metadata.tables[name_of_authors_table]])
+        select_statement = select([mapper_registry.metadata.tables[name_of_episodes_table]])
         result = connection.execute(select_statement)
 
         all_episodes = []
         for row in result:
-            all_episodes.append(row['title'])
+            all_episodes.append(row['episode_id'])
 
         print(all_episodes)
         print(len(all_episodes))
         print(':))))')
 
-
-        assert all_episodes[1] == 'Finding yourself in the character by justifying your actions'
+        # assert all_episodes[1] == 'Finding yourself in the character by justifying your actions'
         assert all_episodes[2] == 'Episode 182 - Lyrically Weak', 'Week 16 Day 5'
         assert len(all_episodes) == 5633
-
